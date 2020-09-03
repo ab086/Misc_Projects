13f89877b7f236957eb984d2760a9c2a12590f22
Austin Blair <44411352+ab086@users.noreply.github.com>
2020-09-03 14:24:01 -0500

    Added comments

--- Box of Random [Python]/NumGuess/main.py
+++ Box of Random [Python]/NumGuess/main.py
@@ -1,17 +1,21 @@
 import random
-import traceback
+#import traceback
 
+# Generate the number
 int_num = random.randrange(1, 101)
 int_attempts = 7
 
 print("A random number be tween 1 and 100 has been generated.")
 
+# Start the guessing loop
 while int_attempts != 0:
     print("Enter your input:")
 
     try:
+        # Grab input
         int_input = int(input())
 
+        # Check if too low or too high
         if int_input < int_num:
             print("Too low! Try again.")
             int_attempts -= 1
@@ -24,9 +28,12 @@ while int_attempts != 0:
             print("Attempts remaining: " + str(int_attempts))
             print() # Blank line
 
+        # If neither, then answer is correct
         else:
             print("Correct!")
             int_attempts = 0
+            
+    # Catch anything other than a number
     except Exception as e:
         print("Error: {}".format(type(e).__name__))
         print("Only numbers are allowed.")
